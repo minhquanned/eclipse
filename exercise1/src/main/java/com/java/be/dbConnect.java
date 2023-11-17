b4557de29448946f0eee0d563f1e458d6ed22db4
Minh Quan <Minh Quan@DESKTOP-J7MRH1Q>
2023-11-17 20:20:26 +0700

    fix eror java.lang.ClassNotFoundException: com.microsoft.sqlserver.jdbc.SQLServerDriver

--- exercise1/src/main/java/com/java/be/dbConnect.java
+++ exercise1/src/main/java/com/java/be/dbConnect.java
@@ -7,45 +7,17 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class dbConnect {
-	public static void main(String[] args) throws SQLException {
-		Connection c = null;
-		ResultSet rs = null;
-		
-		try {
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			String url = "jdbc:sqlserver://DESKTOP-J7MRH1Q:1433;encrypt=true;trustServerCertificate=true;databaseName=LTW;integratedSecurity=true;";
-			String user = "sa";
-			String password = "123456";
-			
-			c = DriverManager.getConnection(url, user, password);
-			Statement stm = c.createStatement();
-			
-			String SQL = "select * from [USER]";
-			
-			System.out.println("connected!");
-			
-			rs = stm.executeQuery(SQL);
-			
-			while(rs.next()) {
-				System.out.println(rs.getString("firstName") + " " + rs.getString("lastName") + " " + rs.getString("doB")
-						 + " " + rs.getString("poB") + " " + rs.getString("sex"));
-			}
-			
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			System.out.println("connection failed!");
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		Connection c = dbConnect.initializeDatabase();
 	}
 	
 	protected static Connection initializeDatabase() throws ClassNotFoundException, SQLException {
-		String dbDriver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-		String dbURL = "jdbc:sqlserver://DESKTOP-J7MRH1Q:1433;encrypt=true;trustServerCertificate=true;integratedSecurity=true;";
-		String dbName = "LTW";
-		String dbUsername = "sa";
-		String dbPassword = "123456";
-		Class.forName(dbDriver);
-		Connection con = DriverManager.getConnection(dbURL + dbName, dbUsername, dbPassword);
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		String url = "jdbc:sqlserver://DESKTOP-J7MRH1Q:1433;encrypt=true;trustServerCertificate=true;databaseName=LTW;integratedSecurity=true;";
+		String user = "sa";
+		String password = "123456";
+		Connection con = DriverManager.getConnection(url, user, password);
+		System.out.println("connected!");
 		return con;
 	}
 }
--- exercise1/src/main/java/com/java/be/userServlet.java
+++ exercise1/src/main/java/com/java/be/userServlet.java
@@ -1,8 +1,8 @@
 package com.java.be;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import jakarta.servlet.ServletException;
@@ -13,35 +13,43 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = {"/userConfirm","/userConfirm/*"})
 public class userServlet extends HttpServlet {
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-		
-		String fName = req.getParameter("firstName");
+    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        // Get data from index.jsp
+        String fName = req.getParameter("firstName");
         String lName = req.getParameter("lastName");
         String DoB = req.getParameter("DoB");
         String PoB = req.getParameter("PoB");
         String Sex = req.getParameter("Sex");
         String[] freeDay = req.getParameterValues("freeDay");
-        
+
         try {
-			Connection con = dbConnect.initializeDatabase();
-			
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
-        PrintWriter printWriter = res.getWriter();
-        
-        // Store data in User object
-        User user = new User(fName,lName,DoB,PoB,Sex,freeDay);
-		
-        req.setAttribute("user", user);
-        String url = "/confirm.jsp";
-        getServletContext().getRequestDispatcher(url).forward(req, res);
-        
-		printWriter.close();
-	}
+            // Connect to database
+            Connection c = dbConnect.initializeDatabase();
+
+            // Execute query
+            String sql = "insert into [USER] (firstName, lastName, doB, poB, sex) values (?, ?, ?, ?, ?)";
+            PreparedStatement stm = c.prepareStatement(sql);
+
+            stm.setString(1, fName);
+            stm.setString(2, lName);
+            stm.setString(3, DoB);
+            stm.setString(4, PoB);
+            stm.setString(5, Sex);
+
+            stm.executeUpdate();
+            stm.close();
+            c.close();
+
+            String url = "/confirm.jsp";
+            getServletContext().getRequestDispatcher(url).forward(req, res);
+
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
